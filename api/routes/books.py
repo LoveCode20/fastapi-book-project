c51b4757148bcf353234adaddc98addcb869cb33
Ubuntu <ubuntu@ip-172-31-3-143.us-east-2.compute.internal>
2025-02-14 20:45:13 +0000

    fix missing book_id

--- api/routes/books.py
+++ api/routes/books.py
@@ -38,16 +38,18 @@ db.books = {
 async def create_book(book: Book):
     db.add_book(book)
     return JSONResponse(
-        status_code=status.HTTP_201_CREATED, content=book.model_dump()
-    )
+        status_code=status.HTTP_201_CREATED, content=book.model_dump(),
+	)
 
-
-@router.get(
-    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
-)
-async def get_books() -> OrderedDict[int, Book]:
-    return db.get_books()
-
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Book not found"
+        )
+    return book
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
